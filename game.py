388b04746e4e9f1d1d5906b7607c339eff5d4fc6
matthew-pryor <pryor.matthew.a@gmail.com>
2022-02-07 13:51:34 -0600

    Added a framework of funtions to the Game class.

---
+++ game.py
@@ -0,0 +1,22 @@
+class Game:
+    
+    def __init__(self) -> None:
+        pass
+
+    def introduction(self): #displays rules and asks the user if they want to play single player or multiplayer.
+        pass
+
+    def run_game(self): #runs all functions.
+        pass
+
+    def round(self): #keeps track of wins - while loop until wins for either player == 2.
+        pass
+
+    def player_1(self): #picks from the list of options
+        pass
+
+    def player_2(self): #picks from the list. If they choose singleplayer in the welcome, then AI overrides and just picks randomly from the list of options.
+        pass
+
+    def display_winner(self): #once two wins are achieved by either player, this is displayed.
+        pass
