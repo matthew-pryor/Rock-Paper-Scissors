9bbb5947af5683e9f47f1648ee13ed47e55935d3
matthew-pryor <pryor.matthew.a@gmail.com>
2022-02-07 11:57:43 -0600

    Added missing files.

---
+++ main.py
@@ -0,0 +1,3 @@
+# we want to do a method override. Both child classes should be able to do the thing but in a slightly different way.
+# AI or other player. Choose gesture method. child classes need choose gesture. method override in both child classes.
+# put everything (options) in a list in the parent class
