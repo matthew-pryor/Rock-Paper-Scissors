6d2485aeb948928d3af24f0b73b46827c16596f5
Josh Kellhofer <sasukekorlo@yahoo.com>
2022-02-07 21:33:30 -0800

    Added selection def for ai (not tested yet) and added input for 2nd player if 2 players are chosen. Added text to represent 2nd player in game and winner function. Winner function algorithm tested and works.

--- ai.py
+++ ai.py
@@ -1,7 +1,16 @@
-class Ai:
+from player import Player
+import random
+
+class Ai(Player):
 
     def __init__(self) -> None: #dont forget superinit
-        pass
+        
+        super().__init__()
+        
+        self.name = "AI"
 
-    def random_ai_selection():
-        pass
+    # def random_ai_selection(self):
+        
+    #     self.random_selection = random.choice(self.list_of_gestures)
+        
+    #     return self.random_selection
--- game.py
+++ game.py
@@ -6,6 +6,7 @@ class Game:
     def __init__(self) -> None:
         
         self.player_1 = Human()
+        self.ai = Ai()
 
     def introduction(self): #displays rules and asks the user if they want to play single player or multiplayer. void
 
@@ -13,14 +14,14 @@ class Game:
 
         self.player_1.set_name()
 
-        print(f'Welcome to the game {self.player_1.name}')
+        print(f'Welcome to the game {self.player_1.name}\n')
 
         while player_1_input_valid is False:
 
             player_1_input = input('Would you like to play against an opponent or by yourself: Press [1] to play with a friend or press [2] play against the CPU. ')
 
             if(player_1_input == '1'):
-
+                self.player_2_name = input("What is the 2nd player's name?\n")
                 self.player_2 = Human()
                 player_1_input_valid = True
 
@@ -48,7 +49,7 @@ class Game:
             if ((self.player_1.wins == 2) or (self.player_2.wins == 2)):
                 
                 self.winner = True
-
+            
             else:
 
                 self.winner = False
@@ -66,99 +67,99 @@ class Game:
     def compare_gestures(self):
 
         if (self.player_1.chosen_gesture == 'Rock' and self.player_2.chosen_gesture == 'Scissors'):               #Rock curshes scissors
-            print(f'Rock crushes scissors! {self.player_1.name} wins!')
+            print(f'Rock crushes scissors! {self.player_1.name} wins!\n')
             self.player_1.wins += 1
         
         elif (self.player_2.chosen_gesture == 'Rock' and self.player_1.chosen_gesture == 'Scissors'):
-            
+            print(f'Rock crushes scissors! {self.player_2_name} wins!\n')
             self.player_2.wins += 1
 
         elif (self.player_1.chosen_gesture == 'Rock' and self.player_2.chosen_gesture == 'Lizard'):               #Rock crushes lizard
-            print(f'Rock crushes Lizard! {self.player_1.name} wins!')
+            print(f'Rock crushes Lizard! {self.player_1.name} wins!\n')
             self.player_1.wins += 1
         
         elif (self.player_2.chosen_gesture == 'Rock' and self.player_1.chosen_gesture == 'Lizard'):
-
+            print(f'Rock crushes Lizard! {self.player_2_name} wins!\n')
             self.player_2.wins += 1  
 
         elif (self.player_1.chosen_gesture == 'Scissors' and self.player_2.chosen_gesture == 'Paper'):      
-            print(f'Scissors cuts paper! {self.player_1.name} wins!')    
+            print(f'Scissors cuts paper! {self.player_1.name} wins!\n')    
             
             self.player_1.wins += 1
         
         elif (self.player_2.chosen_gesture == 'Scissors' and self.player_1.chosen_gesture == 'Paper'):
-            
+            print(f'Scissors cuts paper! {self.player_2_name} wins!\n')
             self.player_2.wins += 1
 
         elif (self.player_1.chosen_gesture == 'Scissors' and self.player_2.chosen_gesture == 'Lizard'):               #scissors decapitates lizard
             
-            print(f'Scissors crushes Lizard! {self.player_1.name} wins!')
+            print(f'Scissors crushes Lizard! {self.player_1.name} wins!\n')
             
             self.player_1.wins += 1
         
         elif (self.player_2.chosen_gesture == 'Scissors' and self.player_1.chosen_gesture == 'Lizard'):
-
+            print(f'Scissors crushes Lizard! {self.player_2_name} wins!\n')
             self.player_2.wins += 1
 
         elif (self.player_1.chosen_gesture == 'Paper' and self.player_2.chosen_gesture == 'Rock'):               #paper covers rock
-            print(f'Paper suffocates Rock! {self.player_1.name} wins!')
+            print(f'Paper suffocates Rock! {self.player_1.name} wins!\n')
             self.player_1.wins += 1
         
         elif (self.player_2.chosen_gesture == 'Paper' and self.player_1.chosen_gesture == 'Rock'):
-            
+            print(f'Paper suffocates Rock! {self.player_2_name} wins!\n')
             self.player_2.wins += 1
 
         elif (self.player_1.chosen_gesture == 'Paper' and self.player_2.chosen_gesture == 'Spock'):               #paper disproves spock
-            print(f'Paper overwhelms Spock! {self.player_1.name} wins!')
+            print(f'Paper overwhelms Spock! {self.player_1.name} wins!\n')
             self.player_1.wins += 1
         
         elif (self.player_2.chosen_gesture == 'Paper' and self.player_1.chosen_gesture == 'Spock'):
-
+            print(f'Paper overwhelms Spock! {self.player_2_name} wins!\n')
             self.player_2.wins += 1
 
         elif (self.player_1.chosen_gesture == 'Lizard' and self.player_2.chosen_gesture == 'Spock'):               #lizard poisons spock
-            print(f'Lizard licks Spock to death! {self.player_1.name} wins!')
+            print(f'Lizard licks Spock to death! {self.player_1.name} wins!\n')
             self.player_1.wins += 1
         
         elif (self.player_2.chosen_gesture == 'Lizard' and self.player_1.chosen_gesture == 'Spock'):
-
+            print(f'Lizard licks Spock to death! {self.player_2.name} wins!\n')
             self.player_2.wins += 1
 
         elif (self.player_1.chosen_gesture == 'Lizard' and self.player_2.chosen_gesture == 'Paper'):               #lizard eats paper
-            print(f'Lizard licks paper into a drooly mess! {self.player_1.name} wins!')
+            print(f'Lizard licks paper into a drooly mess! {self.player_1.name} wins!\n')
             self.player_1.wins += 1
         
         elif (self.player_2.chosen_gesture == 'Lizard' and self.player_1.chosen_gesture == 'Paper'):
-
+            print(f'Lizard licks paper into a drooly mess! {self.player_2_name} wins!\n')
             self.player_2.wins += 1
 
         elif (self.player_1.chosen_gesture == 'Spock' and self.player_2.chosen_gesture == 'Scissors'):               #spock smashes scissors
-            print(f'Spock dismantles scissors! {self.player_1.name} wins!')
+            print(f'Spock dismantles scissors! {self.player_1.name} wins!\n')
             self.player_1.wins += 1
         
         elif (self.player_2.chosen_gesture == 'Spock' and self.player_1.chosen_gesture == 'Scissors'):
-
+            print(f'Spock dismantles scissors! {self.player_2_name} wins!\n')
             self.player_2.wins += 1
 
         elif (self.player_1.chosen_gesture == 'Spock' and self.player_2.chosen_gesture == 'Rock'):               #spock vaporizes rock
-            print(f'Spock phasers Rock into dust! {self.player_1.name} wins!')
+            print(f'Spock phasers Rock into dust! {self.player_1.name} wins!\n')
             self.player_1.wins += 1
         
         elif (self.player_2.chosen_gesture == 'Spock' and self.player_1.chosen_gesture == 'Rock'):
-
+            print(f'Spock phasers Rock into dust! {self.player_2_name} wins!\n')
             self.player_2.wins += 1
 
         elif (self.player_2.chosen_gesture == self.player_1.chosen_gesture):
-            print("It's a tie, no one wins.")
+            print("It's a tie, no one wins. Select again.\n")
             self.player_2.wins += 0
 
     def display_winner(self): #once two wins are achieved by either player, this is displayed. void, funtion complete.
         
         if (self.player_1.wins == 2):
-            print(f"{self.player_1.name} has won the game!")
+            print(f"{self.player_1.name} has won the game!\n")
         elif (self.player_2.wins):
-            print(f"{self.player_2.name} has won the game!")
+            print(f"{self.player_2_name} has won the game!\n")
         elif (self.ai.wins == 2):
-            print ("The AI has become self-aware and won the game!")
+            print ("The AI has become self-aware and won the game!\n")
 
         
